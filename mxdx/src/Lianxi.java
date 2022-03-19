8bfce0d5d90b25886618b7a042bbd9c0824ea560
baron <marvinhan884@gmail.com>
2022-03-19 22:01:56 +0800

    2022-3-19 dev->master

---
+++ mxdx/src/Lianxi.java
@@ -0,0 +1,13 @@
+public class Lianxi {
+
+
+    public static void main(String[] args) {
+        Integer integer = Integer.valueOf(10);
+        System.out.println(integer);
+    }
+}
+class Aaa {
+    public String invoke() {
+        return "2";
+    }
+}
